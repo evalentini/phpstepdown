18ea6d7181cdc2a5847951f48808a3fd17e3d7da
Evan Valentini <evan.valentini@gmail.com>
2014-04-07 11:58:23 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phpstepdown::Application.config.secret_token = '32346f849365033d646f301ded41e5da6dca974989c13512fe795bdc9d1e43590fd3a6aabba709a9e2c5e459f79afce966af42716721823a4f28af2be7fe647d'
